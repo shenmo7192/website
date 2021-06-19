6ff9619b0404bb9e8d1835882cca9ab1c39c883f
cht <3237126351@qq.com>
2021-06-19 21:34:48 +0800

    more elegant code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
 import Window from './home/window';
 import Donate from './donate/donate';
+import './styles/styles.css';
 
+import {contents ,url0, url1,title,bottom }from './content/'
 function App(){
+  const [content,setContent] = useState({
+    content : contents,
+    url: url0,
+    english : false,
+    tit: title,
+    bot: bottom
+  });
+  function setEnglish(){
+    if (content.english===false) {
+      setContent({
+        english : true,
+        content : contents,
+        url:url0,
+        tit:title,
+        bot:bottom
+      })
+    }else{
+      setContent({
+        english : false,
+        content : contents,
+        url:url1,
+        tit: title,
+        bot:bottom
+      }
+      )
+    }
+
+  }
+
   return <Router>
+    <div id="header" className="static">
+                <ul>
+
+                    <li onClick={setEnglish}><a>{content.url[4]}</a></li>
+                    <li className="top-logo"><a href="/Home/">{content.url[0]}</a></li>
+                    <li><a href="index.html">{content.url[1]}</a></li>
+                    <li><a href="https://www.deepinos.org">{content.url[2]}</a></li>
+                    <li><a href="https://www.deepinos.org">{content.url[3]}</a></li>
+
+                </ul>
+            </div>
+            <div className="dropdown">
+                <ul>
+                <li className="top-logo"><a href="index.html">{content.url[0]}</a></li>
+                <div className="dropdown2">
+                    <li><a>More</a></li>
+                    <div className="dropdown-content">
+                        <a href="index.html">{content.url[1]}</a>
+                        <a href="https://www.deepinos.org">{content.url[2]}</a>
+                        <a href="https://www.deepinos.org">{content.url[3]}</a>
+                        <a className="dropbtn2" onClick={setEnglish}>{content.url[4]}</a>
+                    </div>
+                </div>
+                </ul>
+            </div>
+
     <div>
     <Switch>
 
       <Route path="/Home">
-          <Window/>
+          <Window pages={content}/>
       </Route>
       <Route path="/Donate">
-        <Donate/>
+        <Donate pages={content}/>
       </Route>
       <Redirect exact to="/Home" from='/' />
     </Switch>
--- src/home/window.js
+++ src/home/window.js
@@ -1,84 +1,28 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Core from '../chat/core'
-import '../styles/styles.css'
 import Block from '../pages/blocks'
 import Blocktop from '../pages/blockstop'
-import {contents ,url0, url1,title,bottom }from '../content/'
-function Window() {
-  const [content,setContent] = useState({
-    content : contents,
-    url: url0,
-    english : false,
-    tit: title,
-    bot: bottom
-  });
-  function setEnglish(){
-    if (content.english===false) {
-      setContent({
-        english : true,
-        content : contents,
-        url:url0,
-        tit:title,
-        bot:bottom
-      })
-    }else{
-      setContent({
-        english : false,
-        content : contents,
-        url:url1,
-        tit: title,
-        bot:bottom
-      }
-      )
-    }
+function Window(props) {
 
-  }
   return <div>
-            <div id="header" className="static">
-                <ul>
-
-                    <li onClick={setEnglish}><a>{content.url[4]}</a></li>
-                    <li className="top-logo"><a href="/Home/">{content.url[0]}</a></li>
-                    <li><a href="index.html">{content.url[1]}</a></li>
-                    <li><a href="https://www.deepinos.org">{content.url[2]}</a></li>
-                    <li><a href="https://www.deepinos.org">{content.url[3]}</a></li>
-
-                </ul>
-            </div>
-            <div className="dropdown">
-                <ul>
-                <li className="top-logo"><a href="index.html">{content.url[0]}</a></li>
-                <div className="dropdown2">
-                    <li><a>More</a></li>
-                    <div className="dropdown-content">
-                        <a href="index.html">{content.url[1]}</a>
-                        <a href="https://www.deepinos.org">{content.url[2]}</a>
-                        <a href="https://www.deepinos.org">{content.url[3]}</a>
-                        <a className="dropbtn2" onClick={setEnglish}>{content.url[4]}</a>
-                    </div>
-                </div>
-                </ul>
-            </div>
-            <Block title ={content.tit[0]}
-                      left1 = {content.tit[1]}
-                      img = {content.tit[2]}/>
-                        <div className="row">
+            <Block title ={props.pages.tit[0]}
+                      left1 = {props.pages.tit[1]}
+                      img = {props.pages.tit[2]}/>
             {
-                  content.content.map(
+                  props.pages.content.map(
                     (item,idx) => <Blocktop key={idx}
                       title={item[0]}
                       left1={item[1]}
                       img={item[2]}/>)
             }
             {
-                  content.bot.map(
+                  props.pages.bot.map(
                     (item,idx) => <Block key={idx}
                       title={item[0]}
                       left1={item[1]}
                       img={item[2]}/>)
             }
 
-            </div>
             <Core/>
         </div>
 }
