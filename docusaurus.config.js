462163555af2b3f5b8c8018ea2d11ffd5e954df3
cht <3237126351@qq.com>
2021-07-23 17:50:45 +0800

    初步尝试切换图标

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,24 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   themeConfig: {
+    colorMode:{
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+            switchConfig: {        
+              // Icon for the switch while in dark mode       
+              darkIcon: '🌙',
+        // CSS to apply to dark icon,        // React inline style object        // see https://reactjs.org/docs/dom-elements.html#style       
+              darkIconStyle: {
+                marginLeft: '2px',        
+              },
+        // Unicode icons such as '\u2600' will work        // Unicode with 5 chars require brackets: '\u{1F602}'       
+              lightIcon: '\u{1F602}',
+              lightIconStyle: {
+                marginLeft: '1px',        
+              },      
+            },
+    },
     navbar: {
       title: '星火商店',
       logo: {
